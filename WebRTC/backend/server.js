138b5e032d16f6bd2633532060d710438682de29
bazylcossac <bazyl.cossac@gmail.com>
2025-03-19 18:34:17 +0100

    real-time chat app

--- WebRTC/backend/server.js
+++ WebRTC/backend/server.js
@@ -4,15 +4,26 @@ import { Server } from "socket.io";
 
 const app = express();
 const httpServer = http.createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
 io.on("connection", (socket) => {
-  console.log("IO running on port 3000! ", socket);
-  io.emit("message", "hello world");
+  console.log("User connected with: ", socket.id);
+
+  socket.on("send-message", (data) => {
+    io.emit("send-message", data);
+  });
 });
 
 httpServer.listen(3000, () => {
   console.log("Server running on port 3000!");
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "server working" });
 });
 
 // io.listen(3000);
--- WebRTC/frontend/src/App.tsx
+++ WebRTC/frontend/src/App.tsx
@@ -1,14 +1,45 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+const socket = io(process.env.SERVER_URL);
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const socket = io();
+  const [messages, setMessages] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState("");
 
-  useEffect(() => {}, []);
+  const handleInputChange = (message: string) => {
+    setInputValue(message);
+  };
+
+  const submitSendMessage = (e) => {
+    e.preventDefault();
+    socket.emit("send-message", inputValue);
+    setInputValue("");
+  };
+
+  useEffect(() => {
+    socket.on("send-message", (message) => {
+      setMessages((prev) => [...prev, message]);
+    });
+    return () => {
+      socket.off("send-message");
+    };
+  }, []);
+
   return (
     <>
       <p>home page</p>
+      <form onSubmit={submitSendMessage}>
+        <input
+          type="text"
+          placeholder="send message"
+          value={inputValue}
+          onChange={(e) => handleInputChange(e.target.value)}
+        />
+        <button type="submit">send message</button>
+      </form>
+      {messages.map((message, i) => (
+        <p key={i}>{message}</p>
+      ))}
     </>
   );
 }
