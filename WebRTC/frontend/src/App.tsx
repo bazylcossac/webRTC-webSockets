4cfde5fcd6a0b0ed244bb154b52b04c3c331089c
bazylcossac <bazyl.cossac@gmail.com>
2025-03-21 13:29:11 +0100

    users can share screen

--- WebRTC/frontend/src/App.tsx
+++ WebRTC/frontend/src/App.tsx
@@ -9,14 +9,17 @@ const constraints = {
 
 function App() {
   const [allDevices, setAllDevices] = useState<MediaDeviceInfo[]>([]);
+  const [isSharingScreen, setIsSharingScreen] = useState(false);
 
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const videoSharingContainer = useRef(null);
 
+  // Change outut device source
   const handleOutputDeviceChange = async (outputDeviceId: string) => {
     await videoRef.current?.setSinkId(outputDeviceId);
-    // console.log(value);
   };
 
+  // Change input device source
   const handleInputDeviceChange = async (inputDeviceId: string) => {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: { deviceId: { exact: inputDeviceId } },
@@ -25,17 +28,26 @@ function App() {
     videoRef.current!.srcObject = stream;
   };
 
+  const handleSharingScreen = async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
+    if (isSharingScreen) {
+      stream.getVideoTracks()[0].enabled = false;
+      return;
+    }
+    videoSharingContainer.current!.srcObject = stream;
+  };
+
   useEffect(() => {
     const getAllConectedDevices = async () => {
       const deivces = await navigator.mediaDevices.enumerateDevices();
       console.log(deivces);
-
       setAllDevices(deivces);
     };
 
     getAllConectedDevices();
   }, []);
 
+  // starts audio & video
   useEffect(() => {
     const getMediaStream = async () => {
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -73,12 +85,26 @@ function App() {
               </option>
             ))}
         </select>
+        <button
+          className="border-1 border-white p-1 rounded-lg hover:bg-white hover:text-black hover:cursor-pointer transition"
+          onClick={() => {
+            handleSharingScreen();
+            setIsSharingScreen(true);
+          }}
+        >
+          {isSharingScreen ? "stop" : "start"} sharing screen
+        </button>
       </div>
 
       <video
-        id="videoEl"
         ref={videoRef}
         className="rotate-y-180 rounded-xl "
+        autoPlay
+        playsInline
+      />
+      <video
+        className="w-full h-[1080px]"
+        ref={videoSharingContainer}
         autoPlay
         playsInline
       />
